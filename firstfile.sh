ecf2bd06077c2bedc63254eb699998f4e22f7854
robertbelly <trobertbell@gmail.com>
2017-11-04 16:38:14 -0500

    random string generation

--- firstfile.sh
+++ firstfile.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-#!/bin/bash
 echo "about to add"
 git pull origin master
-echo "put string here" >> README.md
+
+tmp_pass=0
+if [ $tmp_pass = 0 ]; then
+        tmp_pass=`head -c 10 /dev/random | base64`
+        echo "${tmp_pass:0:10}" #cut to 10 characters after base64 conversion
+fi
+
+echo "$tmp_pass" >> README.md
 git add README.md
 git commit -m "this is important ok"
 git push origin master
