fcbe8b187af6b1677f84736b804bf451c0601fde
Matt <matthew.n.nicholson1@gmail.com>
2017-11-04 16:25:30 -0500

    add more intents

--- alexa_skill.py
+++ alexa_skill.py
@@ -1,34 +1,52 @@
 from flask import Flask
 from flask_ask import Ask, statement, question, session
+import numpy as np
+import os
 
 
 app = Flask(__name__)
 ask = Ask(app, "/github_contribs")
 
-def get_headline():
-    pass
+
+def add_contributions(number):
+    # do a thing here
+    for ii in range(0,number):
+        os.system("firstfile.sh")
+    print("done")
 
 @app.route("/")
 def homepage():
     return "assuh, dude"
 
-
 @ask.launch
 def start_skill():
-    welcome_message = "hello there, isn't Github neat?"
+    welcome_message = "Do you love Github contributions, but hate to actually code?"
     return question(welcome_message)
 
 @ask.intent("YesIntent")
-def add_contributions():
-    # do a thing here
-    message = "I am about to add a ton of contributions"
-    return statement(message)
+def yes_intent():
+    return question("how many contributions would you like?")
 
 @ask.intent("NoIntent")
 def no_intent():
     # do a thing here
-    message = "You fucking snake... don't talk to me or my son ever again."
+    message = "You fucking snake... go crawl back into your hole and die"
     return statement(message)
+
+@ask.intent("HowManyIntent", convert={"Amount": int}, default={"Amount" : "1"})
+def how_many_intent(Amount):
+    print(Amount)
+    add_contributions(Amount)
+
+    return statement("I just added {} contributions... you lazy piece of shit".format(Amount))
+
+@ask.intent("SurpriseMeIntent")
+def surprise_me_intent():
+    number = int(np.random.rand()*10)
+    add_contributions(number)
+
+    return statement("I just added {} contributions... you lazy piece of shit".format(number))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
