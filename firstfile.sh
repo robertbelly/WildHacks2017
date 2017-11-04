28de234bed3a08733ae32230911304a9d9de95a5
robertbelly <trobertbell@gmail.com>
2017-11-04 16:22:57 -0500

    making bash script

--- firstfile.sh
+++ firstfile.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+#!/bin/bash
+echo "about to add"
+git pull origin master
+echo "put string here" >> README.md
+git add README.md
+git commit -m "this is important ok"
+git push origin master
+echo "done adding"
